26bbe37c9471b7238a724828513b09ca9527bfa6
MagdaDaniell <65734631+MagdaDaniell@users.noreply.github.com>
2020-08-23 19:00:26 +0200

    tests added

---
+++ auto_test_1.py
@@ -0,0 +1,32 @@
+from selenium import webdriver
+
+
+def test_fill_forms(company_name, register_email, register_name, register_phone, register_plain_password):
+    driver = webdriver.Chrome(executable_path=r'C:\TestFiles\chromedriver.exe')
+
+    driver.get('https://dev-1.clicktrans.pl/register-test/courier')
+    driver.maximize_window()
+
+    driver.find_element_by_id("user_register_company_name").send_keys(company_name)
+    driver.find_element_by_id("user_register_email").send_keys(register_email)
+    driver.find_element_by_id("user_register_name").send_keys(register_name)
+    driver.find_element_by_id("user_register_phone").send_keys(register_phone)
+    driver.find_element_by_id("user_register_plainPassword").send_keys(register_plain_password)
+
+    driver.find_element_by_id("user_register_settings_agreementRegulations").click()
+    driver.find_element_by_id("user_register_settings_agreementPersonalData").click()
+    driver.find_element_by_id("user_register_settings_agreementMarketing").click()
+    driver.find_element_by_id("user_register_submit").click()
+
+    driver.save_screenshot("screenshot-login.png")
+    driver.quit()
+
+
+if __name__ == '__main__':
+    test_fill_forms(
+        company_name="The Dog",
+        register_email="dog@dog.pl",
+        register_name="Dog Dog",
+        register_phone="555555555",
+        register_plain_password="thedog11",
+    )
